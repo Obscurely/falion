e4144e38327a0e2d8d7fa39e8d783af2a064fe07
Obscurely <adrian.obscurely@protonmail.com>
2023-07-25 05:43:03 +0300

    add extra client headers to prevent rate limit

--- src/search/utils.rs
+++ src/search/utils.rs
@@ -1,4 +1,5 @@
-use rand::distributions::DistString;
+use rand::{distributions::DistString, Rng};
+use reqwest::header;
 
 /// Create a new reqwest client using a randomly generated user-agent.
 /// This is useful so you don't get limited by some websites like duckduckgo.
@@ -11,8 +12,34 @@ use rand::distributions::DistString;
 /// let client = utils::client_with_random_ua();
 /// ```
 pub fn client_with_random_ua() -> reqwest::Client {
+    let mut rng = rand::thread_rng();
+
+    // specific headers to avoid rate limiting
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        "X-Forwarded-Host",
+        header::HeaderValue::from_static("html.duckduckgo.com"),
+    );
+    let ip = format!(
+        "{}.{}.{}.{}",
+        rng.gen_range(70..120),
+        rng.gen_range(70..120),
+        rng.gen_range(70..120),
+        rng.gen_range(70..120)
+    );
+    headers.insert(
+        "X-Forwarded-For",
+        header::HeaderValue::from_str(&ip).unwrap(),
+    );
+    headers.insert("X-Client-IP", header::HeaderValue::from_str(&ip).unwrap());
+    headers.insert(
+        "Origin",
+        header::HeaderValue::from_static("https://html.duckduckgo.com"),
+    );
+
     reqwest::ClientBuilder::new()
-        .user_agent(rand::distributions::Alphanumeric.sample_string(&mut rand::thread_rng(), 16))
+        .user_agent(rand::distributions::Alphanumeric.sample_string(&mut rng, 16))
+        .default_headers(headers)
         .build()
         .unwrap()
 }
