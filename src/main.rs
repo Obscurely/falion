0d26fc38c681f1f41bb8701373890c87252ab793
Obscurely <adrian.obscurely@protonmail.com>
2023-10-17 18:44:10 +0300

    remove broken windows fix

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,12 @@ mod search;
 mod ui;
 mod util;
 
-#[cfg(windows)]
-use winapi::um::processthreadsapi::GetStartupInfoW;
-
-#[cfg(windows)]
-fn is_launched_from_terminal() -> bool {
-    unsafe {
-        let mut si = std::mem::zeroed();
-        GetStartupInfoW(&mut si);
-        
-        // Check if the STARTF_USESHOWWINDOW flag is set
-        if si.dwFlags & winapi::um::winbase::STARTF_USESHOWWINDOW != 0 {
-            // If it's set, it may be launched from a terminal
-            return si.wShowWindow == winapi::um::winuser::SW_SHOW as u16;
-        }
-    }
-    
-    // If the flag is not set, it's likely not launched from a terminal
-    false
-}
-
 /// Main Falion execution
 #[tokio::main]
 async fn main() {
-    if cfg!(windows) {
-        #[cfg(windows)]
-        if is_launched_from_terminal() {
-            cli::cli().await;
-        } else {
-            ui::ui();
-        }
-        
+    if stdout().is_terminal() {
+        cli::cli().await;
     } else {
-        if stdout().is_terminal() {
-            cli::cli().await;
-        } else {
-            ui::ui();
-        }
+        ui::ui();
     }
 }
